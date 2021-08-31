61ad7135831af1f151644ada64146119afd6d88f
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-08-31 11:41:30 +0100

    Interactions recorded including date, credit, debit and balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,13 +1,17 @@
 class Bank
 
-  attr_reader :balance
+  TODAY = Date.today.strftime("%d/%m/%Y")
+
+  attr_reader :balance, :interactions
 
   def initialize
     @balance = 0
+    @interactions = []
   end
 
   def deposit(amount)
     @balance += amount
+    @interactions << { date: TODAY, credit: amount, debit: 0, balance: @balance }
   end
 
   def withdraw(amount)
@@ -15,6 +19,7 @@ class Bank
       raise 'Insufficient funds!'
     else
       @balance -= amount
+      @interactions << { date: TODAY, credit: 0, debit: amount, balance: @balance }
     end
   end
 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -11,6 +11,12 @@ describe Bank do
     it 'puts money into bank account and changes balance' do
       expect { subject.deposit(100) }.to change { subject.balance }.by(100)
     end
+    it 'adds the date, credit, debit into the interaction' do
+      subject.deposit(50)
+      expect(subject.interactions[0][:date]).to eq Bank::TODAY
+      expect(subject.interactions[0][:credit]).to eq 50
+      expect(subject.interactions[0][:debit]).to eq 0
+    end
   end
 
   describe '#withdraw' do
@@ -20,6 +26,14 @@ describe Bank do
     end
     it 'raises error if you do not have sufficient funds' do
       expect { subject.withdraw(10) }.to raise_error 'Insufficient funds!'
+    end
+    it 'adds the date, debit and balance into the interaction' do
+      subject.deposit(50)
+      subject.withdraw(30)
+      expect(subject.interactions[1][:date]).to eq Bank::TODAY
+      expect(subject.interactions[1][:credit]).to eq 0
+      expect(subject.interactions[1][:debit]).to eq 30
+      expect(subject.interactions[1][:balance]).to eq 20
     end
   end
 
