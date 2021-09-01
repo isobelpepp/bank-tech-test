9f3009021172783a02f59482692ffce9bb2bf5e2
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-09-01 11:51:55 +0100

    refactor

--- lib/statement.rb
+++ lib/statement.rb
@@ -20,12 +20,7 @@ class Statement
 
   def save(filename)
     file = File.open(filename, 'w')
-    @transactions.each do |interaction|
-      interaction_data = [interaction[:date], interaction[:credit],
-                          interaction[:debit], interaction[:balance]]
-      file_line = interaction_data.join(',')
-      file.puts(file_line)
-    end
+    write_to_file(file)
     puts "Your statement has been saved to '#{filename}'."
     file.close
   end
@@ -34,15 +29,30 @@ class Statement
     raise 'That file does not exist' unless File.exist?(filename)
 
     file = File.open(filename, 'r')
-    file.readlines.each do |line|
-      date, credit, debit, balance = line.chomp.split(',')
-      @transactions <<
-        { date: date, credit: credit.to_f, debit: debit.to_f, balance: balance.to_f }
-    end
+    read_from_file(file)
     file.close
   end
 
   def final_balance
     @transactions[-1][:balance]
+  end
+
+  private
+
+  def write_to_file(file)
+    @transactions.each do |interaction|
+      interaction_data = [interaction[:date], interaction[:credit],
+                          interaction[:debit], interaction[:balance]]
+      file_line = interaction_data.join(',')
+      file.puts(file_line)
+    end
+  end
+
+  def read_from_file(file)
+    file.readlines.each do |line|
+      date, credit, debit, balance = line.chomp.split(',')
+      @transactions <<
+        { date: date, credit: credit.to_f, debit: debit.to_f, balance: balance.to_f }
+    end
   end
 end
